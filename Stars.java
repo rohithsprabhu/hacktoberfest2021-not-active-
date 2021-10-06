c3f1a2d89bcb57e551fb0487b07a3190cbff5cbf
Shrithi-ch <85306255+Shrithi-ch@users.noreply.github.com>
2021-10-06 20:48:20 +0530

    Stars.java

---
+++ Stars.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ class Stars
+ {
+   public static void main(String args[])
+   { 
+     int n=5;
+     for(int i=0;i<n;i++){
+     for(int j=0;j<=i;j++)
+     {
+    System.out.println("*");
+   }
+   System.out.println();
+ }
+}
+}
