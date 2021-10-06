6d684781b6c6f98d2c6fb7bb1404cda386080f44
Santhrupthi Suresh <91835471+Santhrupthi2001@users.noreply.github.com>
2021-10-06 12:47:57 +0530

    uppercase or not.py

---
+++ uppercase or not.py
@@ -0,0 +1,7 @@
+# Python Program to check character is Uppercase or not
+ch = input("Please Enter Your Own Character : ")
+
+if(ch.isupper()):
+    print("The Given Character ", ch, "is an Uppercase Alphabet")
+else:
+    print("The Given Character ", ch, "is Not an Uppercase Alphabet")
