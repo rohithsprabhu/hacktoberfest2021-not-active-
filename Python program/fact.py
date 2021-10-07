47abae77a1f1c1627f2f28a8ee9ee0d27152f668
jaishubh <42762557+jaishubh@users.noreply.github.com>
2021-10-07 23:27:25 +0530

    Update fact.py

--- Python program/fact.py
+++ Python program/fact.py
@@ -1,15 +1,15 @@
-def factorial(n):
+def factorialOfNumber(n):
     if n < 0:
         return 0
     elif n == 0 or n == 1:
         return 1
     else:
-        fact = 1
+        ans = 1
         while(n > 1):
             fact *= n
             n -= 1
-        return fact
+        return ans
  
-my_num = 5;
-print("Factorial of",my_num,"is",
-factorial(my_num))
+print("Enter the number to find its Factorial")
+a=int(input())
+print("Factorial of",a,"is",factorial(a))
