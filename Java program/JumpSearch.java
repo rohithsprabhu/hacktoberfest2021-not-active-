85b1b4e7f9988338561f71564f41e213b82e586a
jcasteele <john.casteele@outlook.com>
2021-10-12 13:08:04 -0500

    Added 'if name == main' modular file
    Added a file to provide an example of an "if __name__ == '__main__'" block to help beginning programmers create more functional modular scripts

--- Java program/JumpSearch.java
+++ Java program/JumpSearch.java
@@ -1,11 +1,15 @@
+// Java program to implement Jump Search.
 public class JumpSearch
 {
 	public static int jumpSearch(int[] arr, int x)
 	{
 		int n = arr.length;
 
+		// Finding block size to be jumped
 		int step = (int)Math.floor(Math.sqrt(n));
 
+		// Finding the block where element is
+		// present (if it is present)
 		int prev = 0;
 		while (arr[Math.min(step, n)-1] < x)
 		{
@@ -15,20 +19,26 @@ public class JumpSearch
 				return -1;
 		}
 
+		// Doing a linear search for x in block
+		// beginning with prev.
 		while (arr[prev] < x)
 		{
 			prev++;
 
+			// If we reached next block or end of
+			// array, element is not present.
 			if (prev == Math.min(step, n))
 				return -1;
 		}
 
+		// If element is found
 		if (arr[prev] == x)
 			return prev;
 
 		return -1;
 	}
 
+	// Driver program to test function
 	public static void main(String [ ] args)
 	{
 		int arr[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21,
