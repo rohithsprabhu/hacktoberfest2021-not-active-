e77aaf376224b0364d80306849268ddef3ca6f8e
hv4114 <92599851+hv4114@users.noreply.github.com>
2021-10-16 22:30:31 +0530

    Frequency_of_each_number_in_a_big_number.py
    I created a script to find frequency each number in a big number.
    Please provide approval.
    Thanks

---
+++ Python program/Frequency_of_each_number_in_a_big_number.py
@@ -0,0 +1,22 @@
+print("/Enter any number to find  frequency of particular numbers/")
+n=input("Enter any number : ")
+if('0' in n):
+	print("Frequency of 0 is" ,n.count('0'))
+if('1' in n):
+	print("Frequency of 1 is" ,n.count('1'))
+if('2' in n):
+	print("Frequency of 2 is" ,n.count('2'))
+if('3' in n):
+	print("Frequency of 3 is" ,n.count('3'))
+if('4' in n):
+	print("Frequency of 4 is" ,n.count('4'))
+if('5' in n):
+	print("Frequency of 5 is" ,n.count('5'))
+if('6' in n):
+	print("Frequency of 6 is" ,n.count('6'))
+if('7' in n):
+	print("Frequency of 7 is" ,n.count('7'))
+if('8' in n):
+	print("Frequency of 8 is" ,n.count('8'))
+if('9' in n):
+	print("Frequency of 9 is" ,n.count('9'))
