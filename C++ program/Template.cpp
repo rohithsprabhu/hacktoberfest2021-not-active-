66b438d9da569e7e1689736938bca0d9776c1f81
Sultan <73010596+sultanrif@users.noreply.github.com>
2021-10-17 13:59:10 +0300

    Create Template.cpp
    C++ template for beginners

---
+++ C++ program/Template.cpp
@@ -0,0 +1,33 @@
+// C++ program to demonstrate the use of class templates
+
+#include <iostream>
+using namespace std;
+
+// Class template
+template <class T>
+class Number {
+   private:
+    // Variable of type T
+    T num;
+
+   public:
+    Number(T n) : num(n) {}   // constructor
+
+    T getNum() {
+        return num;
+    }
+};
+
+int main() {
+
+    // create object with int type
+    Number<int> numberInt(7);
+
+    // create object with double type
+    Number<double> numberDouble(7.7);
+
+    cout << "int Number = " << numberInt.getNum() << endl;
+    cout << "double Number = " << numberDouble.getNum() << endl;
+
+    return 0;
+}
