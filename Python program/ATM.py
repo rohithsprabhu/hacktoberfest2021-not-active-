64bb3f2084a43eb3ec1695de41d5204b775173c2
sanjayballavgoswami <93480928+sanjayballavgoswami@users.noreply.github.com>
2021-10-31 20:31:58 +0530

    added comment line

--- Python program/ATM.py
+++ Python program/ATM.py
@@ -4,6 +4,7 @@ if X is a multiple of 5, and Pooja's account balance has enough cash to perform 
 (including bank charges). For each successful withdrawal the bank charges 0.50 $US.
 Calculate Pooja's account balance after an attempted transaction.
 """
+#This is a pyhon program
 withdraw,initialbalance=map(float,input().split())
 if(withdraw<initialbalance and withdraw%5==0):
     print(initialbalance-withdraw-0.5)
